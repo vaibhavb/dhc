af7c679b539d0ad87b48bdf10d3dd833c7b20b07
Vaibhav Bhandari <vaibhavb@gmail.com>
2023-05-17 23:31:35 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+</script>
+
+<template>
+  <div class="logo-container">
+    <img alt="Lib13 logo" src="./assets/logo.png" class="logo"/>
+  </div>
+  <div class="landing">
+    <img alt="Vue logo" src="./assets/DHC-landing.png"/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  align-items: center;
+  margin-top: 60px;
+}
+.logo-container {
+  background-color: #ffffff; /* Set a background color for the container */
+  display: block;
+  align-items: center;
+  justify-content: center;
+}
+.logo {
+  width: 75px; /* Adjust the width and height as per your requirements */
+  filter: brightness(2);
+}
+.landing img{
+  width: 480px;
+}
+</style>
---
+++ src/components/TestApp.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  name: String
+})
+
+</script>
+
+<template>
+    <p>Hello {{ props.name }}</p>
+</template>
+
+<style scoped>
+p {
+    color: red;
+}
+</style>
